c58b9580d7be52373ec05249f42bbf0796557feb
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-02-03 12:33:44 +0200

    remove commented-out code from Main method

--- Problem-Solving/Program.cs
+++ Problem-Solving/Program.cs
@@ -9,17 +9,7 @@ class Program
 
     static void Main(string[] args)
     {
-        ValidPointers VP = new();
-        string s;
-        // s = VP.IsPalindrome("A man, a plan, a canal: Panama");
-        s = VP.IsPalindrome(",.");
-        System.Console.WriteLine(s);
-
-        s = VP.IsPalindrome("0p");
-        System.Console.WriteLine(s);
-
-        s = VP.IsPalindrome("--==x;;;;;;;;;;;;;;)))))))))))))))))X+++++++++==============");
-        System.Console.WriteLine(s);
+         
 
  
     }
