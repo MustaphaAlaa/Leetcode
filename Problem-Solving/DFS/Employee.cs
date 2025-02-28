5b7c139504b5858df38bfb72ab582b0d1f8aa82e
Mostafa Alaa <40583473+MustaphaAlaa@users.noreply.github.com>
2025-02-28 18:48:22 +0200

    Delete Problem-Solving directory

--- Problem-Solving/DFS/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace Problem_Solving.DFS.LCode_690
-{
-    public class Employee
-    {
-        public int id;
-        public int importance;
-        public IList<int> subordinates;
-    }
-}
